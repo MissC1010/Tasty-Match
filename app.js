2777688c9a2e4b5d69ad65e1cd618748a6f21a23
MissC1010 <celina.indhur@hotmail.com>
2024-02-07 12:56:30 +0200

    pushed to git

---
+++ app.js
@@ -0,0 +1,117 @@
+const cardArray = [
+  {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
+  },
+  {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
+  },
+];
+
+//sort is a method with added arrow function
+//math.random generates decimals between 0 and 1(excluding 1)
+// by using 0.5 - math.random it creates positive and negative value decimals
+// giving equal chance to all cards and distribution 
+//if used whole number instead of 0.5 it would favor positive numbers causing bias in distribution
+cardArray.sort(() => 0.5 - Math.random());
+
+//# means looking for id
+const gridDisplay = document.querySelector("#grid");
+const resultDisplay = document.querySelector("#result");
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
+
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "images/blank.png");
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", flipCard);
+    gridDisplay.append(card);
+  }
+}
+
+createBoard();
+
+function checkMatch() {
+  const cards = document.querySelectorAll("img");
+  const [optionOneId, optionTwoId] = cardsChosenIds;
+
+  if (cardsChosen[0] === cardsChosen[1]) {
+    alert("You found a match!");
+    cards[optionOneId].setAttribute("src", "images/white.png");
+    cards[optionTwoId].setAttribute("src", "images/white.png");
+    cards[optionOneId].removeEventListener("click", flipCard);
+    cards[optionTwoId].removeEventListener("click", flipCard);
+    cardsWon.push(cardsChosen);
+  } else {
+    alert("Sorry, try again!");
+
+    setTimeout(() => {
+      cards[optionOneId].setAttribute("src", "images/blank.png");
+      cards[optionTwoId].setAttribute("src", "images/blank.png");
+    }, 500);
+  }
+
+  resultDisplay.innerHTML = cardsWon.length;
+  cardsChosen = [];
+  cardsChosenIds = [];
+
+  if (cardsWon.length === cardArray.length / 2) {
+    resultDisplay.innerHTML = "Congratulations, you found them all!";
+  }
+}
+
+function flipCard() {
+  const cardId = this.getAttribute("data-id");
+  cardsChosen.push(cardArray[cardId].name);
+  cardsChosenIds.push(cardId);
+  console.log(cardsChosen);
+  console.log(cardsChosenIds);
+  this.setAttribute("src", cardArray[cardId].img);
+  if (cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
